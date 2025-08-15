af985aa6fad44917f63f58d202af01a083568000
Nafas Ebrahimi <ebranafas@gmail.com>
2025-08-15 12:29:31 +0330

    remove cached temp_convert sh file

--- temp_convert_1755246235620.sh
+++
@@ -1,4 +0,0 @@
-# convert to MP3
-ffmpeg -i /home/nafas/Projects/video-to-sound/uploads/1755246235620_2025-08-15 11-53-32.mkv \
-        -vn -acodec libmp3lame -q:a 4 \
-        /home/nafas/Projects/video-to-sound/downloads/1755246235620_2025-08-15 11-53-32.mp3
